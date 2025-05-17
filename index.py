9028ffc4b586e26fdbc64b025972481d828fcb22
thiago-888 <thiagodev08@gmail.com>
2025-05-17 18:51:55 -0300

    POO - Exemplos

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+'''
 carro1 = {
     "marca": "Fiat",
     "modelo": "Uno",
@@ -5,3 +6,16 @@ carro1 = {
 }
 
 print(carro1)
+'''
+
+class Carro:
+    def __init__(self, marca, modelo, ano):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+
+uno = Carro("Fiat", "Uno", "2001")
+gol = Carro("Volkswagen", "Fiat", "2012")
+
+print(uno.ano)
+print(gol.marca)
